56b2888cbe74370413cbb868e27ffd9088e7d3ae
joksina <jksalaudeen@yahoo.com>
2015-11-10 13:20:35 -0800

    adding models and views

---
+++ modelAndView.js
@@ -0,0 +1,43 @@
+//Adding a checkbox
+var TodoView = Backbone.View.extend({
+	template: _.template('<h3>' + '<input type=checkbox '+ '<% if (status === "complete") print("checked") %/>' + '<%= description %></h3>'),
+
+	render: function() {
+		// body...
+		this.$el.html(this.template(this.model.toJSON()));
+	}
+})
+
+//update model on UI event
+var TodoView = Backbone.View.extend({
+	events: {
+		'change input': 'toggleStatus'
+	},
+	//rerender
+	initialize: function(){
+		this.model.on('change', this.render, this);
+		this.model.on('destroy', this.render, this);
+	},
+	toggleStatus: function(){
+		this.model.toggleStatus();
+	},
+	render: function(){
+		this.$el.html(this.template(this.model.toJSON()));
+	},
+	remove: function(){
+		this.$el.remove();
+	}
+});
+//go to model to define the function
+var TodoItem = Backbone.Model.extend({
+	toggleStatus: function(){
+		if(this.get('status') === 'incomplete'){
+			this.set({'status': 'complete'});
+		}else{
+			this.set({'status': 'incomplete'});
+		}
+		//to sync changes to server
+		this.save();
+	}
+});
+
